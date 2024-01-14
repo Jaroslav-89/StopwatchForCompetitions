d5fdaffbae99f78b58df8aef9c75fca75746017c
Jaroslav-89 <107784053+Jaroslav-89@users.noreply.github.com>
2024-01-14 12:13:27 +0400

    Initial commit

---
+++ app/src/main/java/com/example/stopwatchforcompetitions/app/App.kt
@@ -0,0 +1,4 @@
+package com.example.stopwatchforcompetitions.app
+
+class App {
+}
---
+++ app/src/main/java/com/example/stopwatchforcompetitions/root/RootActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stopwatchforcompetitions.root
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stopwatchforcompetitions.R
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+}

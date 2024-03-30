d717ce0d0a3a08cac5fa0b05ed35a8504513d603
Jaroslav-89 <107784053+Jaroslav-89@users.noreply.github.com>
2024-03-30 19:27:41 +0400

    добавил ссылку поделиться приложением в нашстор

--- app/src/main/java/com/jaroapps/stopwatchforcompetitions/data/navigation/impl/ExternalNavigatorImpl.kt
+++ app/src/main/java/com/jaroapps/stopwatchforcompetitions/data/navigation/impl/ExternalNavigatorImpl.kt
@@ -3,6 +3,7 @@ package com.jaroapps.stopwatchforcompetitions.data.navigation.impl
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import com.jaroapps.stopwatchforcompetitions.R
 import com.jaroapps.stopwatchforcompetitions.data.navigation.ExternalNavigator
 import com.jaroapps.stopwatchforcompetitions.domain.model.EmailData
 
@@ -10,7 +11,7 @@ class ExternalNavigatorImpl(private val context: Context) : ExternalNavigator {
     override fun shareLink(url: String) {
         Intent().apply {
             action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, url)
+            putExtra(Intent.EXTRA_TEXT, context.getString(R.string.share_text) + url)
             type = "text/plain"
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             context.startActivity(this)
